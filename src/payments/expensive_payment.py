c69d5c404955a77b648c65feb36d18efd823703a
sahilomatic360 <sahilomatic180@gmail.com>
2020-10-21 10:28:17 +0530

    Update expensive_payment.py

--- src/payments/expensive_payment.py
+++ src/payments/expensive_payment.py
@@ -1,4 +1,7 @@
 class ExpensivePaymentGateway:
+    ''' respective software development kit of companies can be used , which will involve 
+    methods like creating connection with API , login into system using credential and 
+    perform transaction'''
     def __init__(self, CreditCardNumber, CardHolder, ExpirationDate,
                  SecurityCode, Amount):
         self.credit_card_number = CreditCardNumber
